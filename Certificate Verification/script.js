574784758bd182f04f5674a203a6996bda851172
Anurag Gupta <70202633+anuragoasis@users.noreply.github.com>
2022-01-23 21:11:00 +0530

    Update script.js

--- Certificate Verification/script.js
+++ Certificate Verification/script.js
@@ -8,14 +8,14 @@ const verifyCertificate = function () {
     .then(response => response.json())
     .then(data => {
       if (data.includes(number)) {
-        const container = document.getElementById('container');
+        const container = document.getElementById('validation-box');
         container.innerText = `✅ Verified`;
         container.style.fontSize = '15vmin';
         console.log(container);
         document.body.appendChild(container);
       }
       else {
-        const container = document.getElementById('container');
+        const container = document.getElementById('validation-box');
         container.innerHTML = `❌ <br>Not Verified`;
         container.style.fontSize = '15vmin';
       }
